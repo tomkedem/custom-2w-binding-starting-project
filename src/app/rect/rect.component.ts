c048db8899a9d110f54f5e720743ab574f83523e
Tomer Kedem <tompha1@hotmail.com>
2024-11-26 08:37:14 +0200

    first commit

---
+++ src/app/rect/rect.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-rect',
+  standalone: true,
+  imports: [],
+  templateUrl: './rect.component.html',
+  styleUrl: './rect.component.css',
+})
+export class RectComponent {
+  // Todo: Implement custom two-way binding
+  @Input ({ required: true }) size!: {width: string; height: string};
+  //size = input.required<{width: string; height:string}>()
+  @Output() sizeChange = new EventEmitter<{width: string; height: string}>();
+
+  onReset() {
+    this.sizeChange.emit({
+      width: '200',
+      height: '100'
+    });
+  }
+}
