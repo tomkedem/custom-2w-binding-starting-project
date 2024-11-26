541ebd4aba735e277306e0b2224c48275a29a7ae
Tomer Kedem <tompha1@hotmail.com>
2024-11-26 08:41:53 +0200

    An Easier Way of Setting Up Custom Two-Way Binding

--- src/app/rect/rect.component.ts
+++ src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, input, model, Output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -9,12 +9,13 @@ import { Component, EventEmitter, Input, input, Output } from '@angular/core';
 })
 export class RectComponent {
   // Todo: Implement custom two-way binding
-  @Input ({ required: true }) size!: {width: string; height: string};
-  //size = input.required<{width: string; height:string}>()
-  @Output() sizeChange = new EventEmitter<{width: string; height: string}>();
+ // @Input ({ required: true }) size!: {width: string; height: string};
+//  @Output() sizeChange = new EventEmitter<{width: string; height: string}>();
+  size = model.required<{width: string; height: string}>();
+ 
 
   onReset() {
-    this.sizeChange.emit({
+    this.size.set({
       width: '200',
       height: '100'
     });
